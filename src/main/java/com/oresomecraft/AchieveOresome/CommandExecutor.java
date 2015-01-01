1ebd01c638dbbb6bd8c747212219f73f760adadb
azdaspaz818 <azza_star851@hotmail.com>
2015-01-01 22:48:49 +1100

    Modified the CommandExecutor class, adding documentation

--- src/main/java/com/oresomecraft/AchieveOresome/CommandExecutor.java
+++ src/main/java/com/oresomecraft/AchieveOresome/CommandExecutor.java
@@ -6,6 +6,14 @@ import org.bukkit.command.CommandSender;
  * Created by user on 1/01/2015.
  */
 public class CommandExecutor {
+    /**
+     * Execute the given command.
+     * @param cmd The Command to execute
+     * @param sender The sender of the command
+     * @param label The label of the command
+     * @param args The arguments to execute the command with.
+     * @return True if the command executes correctly, otherwise false.
+     */
     public boolean execute(String cmd, CommandSender sender, String label, String[] args) {
         Command command = Command.createCommand(cmd);
         if (command == null) {
